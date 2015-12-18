952365378c88ab3cfe0cfe71ca35e6608eded135
Francisco Javier Moreno <frajamomo@hotmail.com>
2015-12-18 23:01:36 +0000

    led blinking and temperature sensor code

---
+++ test01_led/led_blink.py
@@ -0,0 +1,14 @@
+# Based in the http://razzpisampler.oreilly.com/ch03.html example
+
+
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.OUT)
+
+while (True):
+    GPIO.output(18, True)
+    time.sleep(0.5)
+    GPIO.output(18, False)
+    time.sleep(0.5)
