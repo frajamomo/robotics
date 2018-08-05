d985f0b8baf7f12a4ff4fb5cb85497d6e44d4ad3
Francisco Javier Moreno <frajamomo@hotmail.com>
2018-08-05 23:44:55 +0200

    arduino servo example

--- test08_beebottle/raspi_dht22.py
+++ test08_beebottle/raspi_dht22.py
@@ -25,7 +25,7 @@ def run():
     ### Assume
     humidity, temperature = Adafruit_DHT.read_retry( Adafruit_DHT.DHT22, pin )
     if humidity is not None and temperature is not None:
-        print "Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperature, humidity)
+#        print "Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperature, humidity)
         try:
           #Send temperature to Beebotte
           temp_resource.write(temperature)
