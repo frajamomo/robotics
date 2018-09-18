4ade4cfe43b6cdc497efd0975a12f50547c3c6c0
Francisco Javier Moreno <frajamomo@hotmail.com>
2018-09-18 13:00:22 +0200

    adding distance metering

---
+++ arduino/test02_hc-sr04/test02_hc-sr04.ino
@@ -0,0 +1,56 @@
+// HC-SR04-test.ino
+//
+// Get distance information from Ultrasonic Ranging Module HC-SR04 and
+// send it via serial port.
+//
+// electronut.in
+
+#include "Arduino.h"
+
+int pinTrigger = 2;
+int pinEcho = 4;
+int led = 13;
+
+void setup()
+{
+  // initialize serial comms
+  Serial.begin(9600);
+
+  // set pins
+  pinMode(pinTrigger, OUTPUT);
+  pinMode(pinEcho, INPUT);
+  pinMode(led, OUTPUT);
+}
+
+void loop()
+{
+  // send a 10us+ pulse
+  digitalWrite(pinTrigger, LOW);
+  delayMicroseconds(20);
+  digitalWrite(pinTrigger, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(pinTrigger, LOW);
+  delayMicroseconds(20);
+
+  //  read duration of echo
+  int duration = pulseIn(pinEcho, HIGH);
+
+  if(duration > 0) {
+    // dist = duration * speed of sound * 1/2
+    // dist in cm = duration in us * 1 x 10^{-6} * 340.26 * 100 * 1/2
+    // =  0.017*duration
+    float dist = 0.017 * duration;
+    Serial.println(dist);
+
+    if (dist < 10.00) {
+      digitalWrite(led, HIGH);
+    }
+    else {
+      digitalWrite(led, LOW);
+    }
+  }
+
+  // wait
+  delay(70);
+}
+
